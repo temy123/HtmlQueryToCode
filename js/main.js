ace31ed678d867f191029d0b5be4ba961763a094
이현우 <ehyunoo@ihyeon-uui-MacBookAir.local>
2021-07-08 23:37:13 +0900

    First Commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+console.log("test");
+
+window.onload = function () {
+  var source_box = document.getElementById("source");
+  var result_box = document.getElementById("result");
+  var button = document.getElementById("submit");
+
+  button.onclick = function () {
+    var source = source_box.value.split("\n");
+
+    result_box.value = "";
+
+    source.forEach((text) => {
+      var index = text.indexOf(":");
+      var key = text.substr(0, index);
+      var value = text.substr(index + 2, 9999).replaceAll(/"/gi, '\\"');
+
+      result_box.value += '"' + key + '": "' + value + '",\n';
+    });
+  };
+};
